b409e8ff5ec062e20d5ab34ac6d430d38111c3b5
Anupeksha <aradhi1217>
2021-02-06 07:59:11 +0530

    second commit

--- src/test/java/HeadLessBrowserTesting/HeadlessBrowser.java
+++ src/test/java/HeadLessBrowserTesting/HeadlessBrowser.java
@@ -8,7 +8,7 @@ import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.htmlunit.HtmlUnitDriver;
+//import org.openqa.selenium.htmlunit.HtmlUnitDriver;
 import org.testng.annotations.Test;
 
 public class HeadlessBrowser 
@@ -33,15 +33,15 @@ public class HeadlessBrowser
 		//		System.setProperty("webdriver.gecko.driver", "/home/anu/Selenium Drivers/geckodriver");
 		//HTMLUnitDriver is not available in Selenium 3
 		//To use this concept, download HTMLUnitDRiver jars
-		WebDriver driver = new HtmlUnitDriver();
-		driver.manage().window().maximize();
-		driver.manage().deleteAllCookies();
-		driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
-		driver.manage().timeouts().pageLoadTimeout(40, TimeUnit.SECONDS);
-
-		driver.get("https://ui.freecrm.com");
-		driver.findElement(By.name(prop.getProperty("username_name"))).sendKeys(prop.getProperty("userName"));
-		driver.findElement(By.name(prop.getProperty("password_name"))).sendKeys(prop.getProperty("Password"));
+//		WebDriver driver = new HtmlUnitDriver();
+//		driver.manage().window().maximize();
+//		driver.manage().deleteAllCookies();
+//		driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+//		driver.manage().timeouts().pageLoadTimeout(40, TimeUnit.SECONDS);
+//
+//		driver.get("https://ui.freecrm.com");
+//		driver.findElement(By.name(prop.getProperty("username_name"))).sendKeys(prop.getProperty("userName"));
+//		driver.findElement(By.name(prop.getProperty("password_name"))).sendKeys(prop.getProperty("Password"));
 	}
 
 }
